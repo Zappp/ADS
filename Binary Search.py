81dec552679107f86790f1a86e0da984d851501f
Zappp <marcinkomar97@gmail.com>
2019-02-05 13:25:48 +0100

    .

--- Binary Search.py
+++ Binary Search.py
@@ -1,4 +1,10 @@
 import random
+from time import time
+import matplotlib.pyplot as wykres
+
+boool = True
+nums = []
+times = []
 
 
 def SzukBin(A, x):
@@ -8,7 +14,7 @@ def SzukBin(A, x):
 
     while l <= p:
         i += 1
-        k = (int)((l + p) / 2)
+        k = int((l + p) / 2)
         if x > A[k]:
             l = k + 1
         elif x < A[k]:
@@ -16,22 +22,45 @@ def SzukBin(A, x):
         else:
             break
 
-    print("Ilość sondowań: ", i)
-
     if A[k] == x:
         return k
     else:
         return 0
 
 
-n = int(input('Podaj długość listy: '))
-x = int(input('Podaj szukaną wartość: '))
+limit = int(input('Podaj ilość list: '))
+while boool:
+    if limit == 0:
+        boool = False
+        break
+    n = random.randrange(2, random.randrange(3, 10000))  # długość początkowa listy
 
-A = random.sample(range(1, 2 * n), n)
-A.sort()
-z = SzukBin(A, x)
-print(A)
-if z == 1:
-    print('SzukBin: 0')
-else:
-    print('SzukBin: ', z, )
+    A = []
+    i = 0
+    for i in range(0, n):
+        A.append(1 + i)
+        i += 1
+
+    for i in range(0, random.randint(1, len(A) - 1)):  # ile wartości wyjetych (max n - 1)
+        z = random.randint(0, len(A) - 1)
+        del A[z]
+
+    x = random.randint(1, A[len(A) - 1])
+
+    timeA = time()
+    b = SzukBin(A, x)
+    timeB = time()
+
+    nums.append(len(A))
+    times.append(timeB - timeA)
+
+    limit -= 1
+
+if True:
+    times = [x for _, x in sorted(zip(nums, times))]
+    nums.sort()
+
+    wykres.plot(nums, times)
+    wykres.ylabel('time')
+    wykres.xlabel('nums')
+    wykres.show()
